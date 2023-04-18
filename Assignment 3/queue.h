dff37d21e053b00a785c3338f4b00d2048718fc0
timcruzer <91102028+timcruzer@users.noreply.github.com>
2023-04-17 23:10:42 -0500

    Update queue.h

--- Assignment 3/queue.h
+++ Assignment 3/queue.h
@@ -1,1 +1,33 @@
+#include <string>
 
+using namespace std;
+
+class Node {
+public:
+    string data;
+    Node* next;
+
+    Node(string value) { // constructor
+        data = value;
+        next = nullptr;
+    }
+
+    ~Node() { // destructor
+        delete next;
+    }
+};
+
+class queue {
+public:
+    void insertRear(string value);
+    string popFront(); // remove front value and subtract queue size
+    string removeFront(); // remove front value
+    bool queueEmpty(); // check if queue empty
+    int queueSize(); // return size
+    queue(); 
+
+private:
+    int queuesize;
+    Node* head;
+    Node* tail;
+};
