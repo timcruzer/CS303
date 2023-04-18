1917faad736859b43c6b25f12d36b25aa54df8c4
timcruzer <91102028+timcruzer@users.noreply.github.com>
2023-04-17 23:11:29 -0500

    Update stack.h

--- Assignment 3/stack.h
+++ Assignment 3/stack.h
@@ -1,1 +1,20 @@
+#include <string>
+#include <stack>
 
+using namespace std;
+
+class Infix{
+    public:
+        
+        bool isOperator(char input); // see if operator
+        bool isOperand(char input); // see if operand
+        bool greaterOrEqual(char operand1, char operand2); // compare priorities
+        
+        int priority(char operand); // see which number for priority
+
+        string convertToPostfix(string infix); // convert to postfix
+
+
+    private:
+        string infix, postfix;
+};
